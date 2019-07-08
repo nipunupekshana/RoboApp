05b9cc4b49f1a077daf8927080479454df045e0e
nipunupekshana <nipunupekshana1@gmail.com>
2019-07-08 18:45:14 +0530

    my robo app

---
+++ build/precache-manifest.b3bd8936f7d9d37eb46bd3959739fcc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffca8ac3b8a2e8833c319254225168a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "770990093ebd534c1532",
+    "url": "/static/css/main.5b7c5fb4.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "770990093ebd534c1532",
+    "url": "/static/js/main.46aa7e34.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
