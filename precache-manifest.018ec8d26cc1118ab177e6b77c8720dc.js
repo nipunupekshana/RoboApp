2a07effdacb2e26efec0413a48a436956baaafd4
nipunupekshana <nipunupekshana1@gmail.com>
2019-07-08 19:16:08 +0530

    Updates

---
+++ precache-manifest.018ec8d26cc1118ab177e6b77c8720dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e128757d1b27f20015e4f5a7ba57b8e",
+    "url": "/RoboApp/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/RoboApp/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "e647ebe435f403468d6d",
+    "url": "/RoboApp/static/css/main.e2ec1c0f.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/RoboApp/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "e647ebe435f403468d6d",
+    "url": "/RoboApp/static/js/main.38b37eb4.chunk.js"
+  },
+  {
+    "revision": "173e0aac9e3d78f5932d",
+    "url": "/RoboApp/static/js/runtime~main.63995c9c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboApp/static/media/SEGA.12a3db8b.woff"
+  }
+]);
